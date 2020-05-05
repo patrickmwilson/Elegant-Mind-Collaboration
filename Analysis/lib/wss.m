33f1edcb7a9de0d63f74133b70597f4b6766d918
Patrick Wilson <patrickmwilson@ucla.edu>
2020-05-04 23:19:11 -0700

    changed weighted least squares optimization, compared with chi-squared minimization and included reduced chi square parameter

--- Analysis/lib/wss.m
+++ Analysis/lib/wss.m
@@ -1,9 +1,6 @@
 function [avgData, wssAvg] = wss(data,name,avg)
     % Average replicate observations at discrete values
-    clear avgData;
-    clear wssAvg;
-    clear('averageData');
-    avgData = averageData(data,(1+(strcmp(name,'T1'))));
+    avgData = averageData(data,(1+(strcmp(name,'Fully Crowded'))));
 
     if(strcmp(name,'Anstis'))
         avgData(:,3) = 0;
@@ -11,18 +8,18 @@ function [avgData, wssAvg] = wss(data,name,avg)
         return;
     end
     
-    clear xvals;
-    clear yvals;
     xvals = avgData(:,1)';
     yvals = avgData(:,2)';
     
-    standard_error = (avgData(:,3)').^2;
+    variance = (avgData(:,3)').^2;
     
     %To avoid error caused by dividing by zero
-    standard_error(standard_error == 0) = 0.000001;
+    variance(variance == 0) = 0.000001;
     
-    % Weights are assumed to be 1/standard error
-    w = 1./standard_error;
+    % Weights are assumed to be 1/standard error, normalized to 0-1
+    w = 1./variance;
+    w = w - min(w(:));
+    w = w./max(w(:));
     
     % Weighted least sum of squares equation to be minimized
     f = @(x,xvals,yvals,w)sum(w.*((yvals-(xvals.*x)).^2));
@@ -32,5 +29,13 @@ function [avgData, wssAvg] = wss(data,name,avg)
     fun = @(x)f(x,xvals,yvals,w);
     xMin = avg*0.5;
     xMax = avg*1.5;
-    wssAvg = fminbnd(fun,xMin,xMax);
+    options = optimset('Display','Iter');
+    [wssAvg, fval] = fminbnd(fun,xMin,xMax, options);
+
+    ss = sum((yvals-(xvals.*wssAvg)).^2);
+    disp(sqrt(ss/length(xvals)));
+
+    disp(sqrt(fval/length(xvals)));
+
+    disp(sqrt(mean(w.*((yvals-(xvals.*wssAvg))).^2)));
 end
