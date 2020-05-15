de6e138fd2fac2cd5bcc0e1f3575a56400ba16e4
patrickmwilson <patrickmwilson@ucla.edu>
2020-05-15 13:01:38 -0700

    Fixed wss logic issue

--- Analysis/lib/wss.m
+++ Analysis/lib/wss.m
@@ -18,24 +18,20 @@ function [avgData, wssAvg] = wss(data,name,avg)
     
     % Weights are assumed to be 1/standard error, normalized to 0-1
     w = 1./variance;
-    w = w - min(w(:));
-    w = w./max(w(:));
     
     % Weighted least sum of squares equation to be minimized
-    f = @(x,xvals,yvals,w)sum(w.*((yvals-(xvals.*x)).^2));
+    f = @(x,xvals,yvals,w)sum(w.*( (yvals-(xvals.*x)).^2 ) );
         
     % fminbnd iteratively searches for the slope parameter which
     % minimizes the WLSS equation
     fun = @(x)f(x,xvals,yvals,w);
     xMin = avg*0.5;
     xMax = avg*1.5;
-    options = optimset('Display','Iter');
-    [wssAvg, fval] = fminbnd(fun,xMin,xMax, options);
-
-    ss = sum((yvals-(xvals.*wssAvg)).^2);
-    disp(sqrt(ss/length(xvals)));
-
-    disp(sqrt(fval/length(xvals)));
-
-    disp(sqrt(mean(w.*((yvals-(xvals.*wssAvg))).^2)));
+    options = optimset('Display','iter');
+    [wssAvg, fval] = fminbnd(fun,xMin,xMax,options);
+    
+    disp("fval");
+    disp(fval);
+    disp("wssAvg");
+    disp(wssAvg);
 end
